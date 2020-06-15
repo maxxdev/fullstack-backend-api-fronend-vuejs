dc5d768ad2a8c82babab6ce1935426b19d1a33a1
Maxx <maksim.skomorokh@gmail.com>
2020-06-15 16:05:37 +0300

    frontend skeleton

---
+++ frontend/src/components/Articles.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="articles">
+    <h1>Articles</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Articles',
+    props: {
+      articles: Array
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Articles msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import Articles from '@/components/Articles.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      Articles
+    }
+  }
+</script>
