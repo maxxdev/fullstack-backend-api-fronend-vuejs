3c3f4e638ea7ddc7132ca1199d7577306a89ffa4
Maxx <maksim.skomorokh@gmail.com>
2020-06-15 16:41:45 +0300

    add loading indicator, filters UI

--- frontend/src/components/Articles.vue
+++ frontend/src/components/Articles.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="articles">
     <h1>Articles</h1>
-    <pre>{{articles}}</pre>
+    <hr>
+    <div class="filter">
+      <form action="">
+        <div class="filter_item">
+          <label for="filter__category">Category</label>:<input type="text" id="filter__category">
+        </div>
+        <div class="filter_item">
+          <label for="filter__title">Title</label>:<input type="text" id="filter__title">
+        </div>
+        <div class="filter_item">
+          <label for="filter__content">Content</label>:<input type="text" id="filter__content">
+        </div>
+        <div class="filter_item">
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    </div>
+    <hr>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <pre>{{articles}}</pre>
+    </div>
   </div>
 </template>
 
@@ -21,5 +42,17 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .filter form {
+    display: flex;
+    justify-content: left;
+    justify-items: left;
+  }
 
+  .filter form .filter_item {
+    padding-left: 10px;
+
+    & label {
+      padding-right: 5px;
+    }
+  }
 </style>
